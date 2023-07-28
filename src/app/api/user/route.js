2923d2c329fda063607589b5bee6d4eb08f1fe9f
Karthik <karthithelearner@gmail.com>
2023-07-28 12:59:27 +0530

    getuser--bug

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/helper/prismaclient";
 
 export async function GET(request) {
+  console.log(request.nextUrl.searchParams.get("foo"));
   try {
     const result = await prisma.user.findMany({
       select: {
